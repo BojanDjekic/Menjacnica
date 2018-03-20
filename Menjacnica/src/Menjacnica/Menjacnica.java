b771439101c4be659c99984bfb7204bacebcb85d
BojanDjekic <djekic7316@fon.bg.ac.rs>
2018-03-19 20:29:04 -0700

    Implementirana do kraja klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

--- Menjacnica/src/Menjacnica/Menjacnica.java
+++ Menjacnica/src/Menjacnica/Menjacnica.java
@@ -1,27 +1,48 @@
 package Menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import MenjacnicaInterfejs.MenjacnicaInterfejs;
+import Valuta.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	@Override
 	public void dodajKurs(GregorianCalendar datum, double kupovni, double prodajni, double srednji, String naziv,
 			String skraceniNaziv) {
-		// TODO Auto-generated method stub
-
+		Valuta nova = new Valuta();
+		nova.setDatum(datum);
+		nova.setKupovni(kupovni);
+		nova.setProdajni(prodajni);
+		nova.setSrednji(srednji);
+		nova.setNaziv(naziv);
+		nova.setSkraceniNaziv(skraceniNaziv);
+		valute.add(nova);
 	}
 
 	@Override
 	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getDatum().equals(datum)) {
+				valute.remove(i);
+				break;
+			}
+		}
 	}
 
 	@Override
 	public double[] vratiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		double[] niz = new double[3];
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getDatum().equals(datum)) {
+				niz[0] = valute.get(i).getKupovni();
+				niz[1] = valute.get(i).getProdajni();
+				niz[2] = valute.get(i).getSrednji();
+				return niz;
+			}
+		}
 		return null;
 	}
 
