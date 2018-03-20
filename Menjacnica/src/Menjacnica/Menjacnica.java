0131984a319807f335124b4b54c2925f547b72ff
BojanDjekic <djekic7316@fon.bg.ac.rs>
2018-03-19 20:25:14 -0700

    Kreirana klasa Menjacnica u okviru paketa Menjacnica koja implementira interfejs MenjacnicaInterfejs

---
+++ Menjacnica/src/Menjacnica/Menjacnica.java
@@ -0,0 +1,28 @@
+package Menjacnica;
+
+import java.util.GregorianCalendar;
+
+import MenjacnicaInterfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, double kupovni, double prodajni, double srednji, String naziv,
+			String skraceniNaziv) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double[] vratiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
