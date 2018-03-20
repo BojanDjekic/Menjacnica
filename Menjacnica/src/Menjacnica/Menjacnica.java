c488fd3983b0aca13946652c7dc52d5b0f2350c3
BojanDjekic <djekic7316@fon.bg.ac.rs>
2018-03-19 20:47:11 -0700

    Implementirana metoda dodajKurs u okviru klase Menjacnica na master grani

--- Menjacnica/src/Menjacnica/Menjacnica.java
+++ Menjacnica/src/Menjacnica/Menjacnica.java
@@ -1,16 +1,24 @@
 package Menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import MenjacnicaInterfejs.MenjacnicaInterfejs;
+import Valuta.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 	@Override
 	public void dodajKurs(GregorianCalendar datum, double kupovni, double prodajni, double srednji, String naziv,
 			String skraceniNaziv) {
-		// TODO Auto-generated method stub
-
+		Valuta novaValuta = new Valuta();
+		novaValuta.setKupovni(kupovni);
+		novaValuta.setProdajni(prodajni);
+		novaValuta.setSrednji(srednji);
+		novaValuta.setSkraceniNaziv(skraceniNaziv);
+		novaValuta.setNaziv(naziv);
+		novaValuta.setDatum(datum);
+		valute.addLast(novaValuta);
 	}
 
 	@Override
